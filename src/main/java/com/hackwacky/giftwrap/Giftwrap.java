9576d55b80cd5645a758758c9300c02b27fd22c2
Samy0_o <97963987+SamyIOoOI@users.noreply.github.com>
2026-01-23 17:08:57 +0200

    Initial commit

---
+++ src/main/java/com/hackwacky/giftwrap/Giftwrap.java
@@ -0,0 +1,13 @@
+package com.hackwacky.giftwrap;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Giftwrap implements ModInitializer {
+    public static final String MOD_ID = "giftwrap";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
+    }
+}

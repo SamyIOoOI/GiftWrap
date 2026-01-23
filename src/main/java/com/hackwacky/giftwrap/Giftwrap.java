3017c4dad1e513749c5cd3eb44316d6cd243f6b5
Samy0_o <97963987+SamyIOoOI@users.noreply.github.com>
2026-01-23 19:55:15 +0200

    structure for new items

--- src/main/java/com/hackwacky/giftwrap/Giftwrap.java
+++ src/main/java/com/hackwacky/giftwrap/Giftwrap.java
@@ -1,5 +1,6 @@
 package com.hackwacky.giftwrap;
 
+import com.hackwacky.giftwrap.items.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,5 +10,6 @@ public class Giftwrap implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
+        ModItems.registerModItems();
     }
 }
--- src/main/java/com/hackwacky/giftwrap/items/ModItems.java
+++ src/main/java/com/hackwacky/giftwrap/items/ModItems.java
@@ -1,4 +1,86 @@
 package com.hackwacky.giftwrap.items;
+import com.hackwacky.giftwrap.Giftwrap;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item GIFT_BUNDLE = registerItem("gift_bundle", new Item(new Item.Settings()));
+    public static final Item GIFT_CHEST = registerItem("gift_chest", new Item(new Item.Settings()));
+    public static final Item GIFT_BLOCK = registerItem("gift_block", new Item(new Item.Settings()));
+    public static final Item GIFT_WRAP = registerItem("gift_wrap", new Item(new Item.Settings()));
+    public static final Item SMALL_GIFT = registerItem("small_gift", new Item(new Item.Settings()));
+    public static final Item MEDIUM_GIFT = registerItem("medium_gift", new Item(new Item.Settings()));
+    public static final Item LARGE_GIFT = registerItem("large_gift", new Item(new Item.Settings()));
+    public static final Item WRAPPED_DIAMOND_SWORD = registerItem("wrapped_diamond_sword", new Item(new Item.Settings()));
+    public static final Item WRAPPED_DIAMOND_AXE = registerItem("wrapped_diamond_axe", new Item(new Item.Settings()));
+    public static final Item WRAPPED_DIAMOND_PICKAXE = registerItem("wrapped_diamond_pickaxe", new Item(new Item.Settings()));
+    public static final Item WRAPPED_DIAMOND_SHOVEL = registerItem("wrapped_diamond_shovel", new Item(new Item.Settings()));
+    public static final Item WRAPPED_GOLDEN_SWORD = registerItem("wrapped_golden_sword", new Item(new Item.Settings()));
+    public static final Item WRAPPED_GOLDEN_AXE = registerItem("wrapped_golden_axe", new Item(new Item.Settings()));
+    public static final Item WRAPPED_GOLDEN_PICKAXE = registerItem("wrapped_golden_pickaxe", new Item(new Item.Settings()));
+    public static final Item WRAPPED_GOLDEN_SHOVEL = registerItem("wrapped_golden_Shovel", new Item(new Item.Settings()));
+    public static final Item WRAPPED_IRON_SWORD = registerItem("wrapped_iron_sword", new Item(new Item.Settings()));
+    public static final Item WRAPPED_IRON_AXE = registerItem("wrapped_iron_axe", new Item(new Item.Settings()));
+    public static final Item WRAPPED_IRON_PICKAXE = registerItem("wrapped_iron_pickaxe", new Item(new Item.Settings()));
+    public static final Item WRAPPED_IRON_SHOVEL = registerItem("wrapped_iron_shovel", new Item(new Item.Settings()));
+    public static final Item WRAPPED_STONE_SWORD = registerItem("wrapped_stone_sword", new Item(new Item.Settings()));
+    public static final Item WRAPPED_STONE_AXE = registerItem("wrapped_stone_axe", new Item(new Item.Settings()));
+    public static final Item WRAPPED_STONE_PICKAXE = registerItem("wrapped_stone_pickaxe", new Item(new Item.Settings()));
+    public static final Item WRAPPED_STONE_SHOVEL = registerItem("wrapped_stone_shovel", new Item(new Item.Settings()));
+    public static final Item WRAPPED_WOODEN_SWORD = registerItem("wrapped_wooden_sword", new Item(new Item.Settings()));
+    public static final Item WRAPPED_WOODEN_AXE = registerItem("wrapped_wooden_axe", new Item(new Item.Settings()));
+    public static final Item WRAPPED_WOODEN_PICKAXE = registerItem("wrapped_wooden_pickaxe", new Item(new Item.Settings()));
+    public static final Item WRAPPED_WOODEN_SHOVEL = registerItem("wrapped_wooden_shovel", new Item(new Item.Settings()));
+    public static final Item WRAPPED_WAND = registerItem("wrapped_wand", new Item(new Item.Settings()));
+    public static final Item WRAPPED_DIAMOND_HOE = registerItem("wrapped_diamond_hoe", new Item(new Item.Settings()));
+    public static final Item WRAPPED_GOLDEN_HOE = registerItem("wrapped_golden_hoe", new Item(new Item.Settings()));
+    public static final Item WRAPPED_IRON_HOE = registerItem("wrapped_iron_hoe", new Item(new Item.Settings()));
+    public static final Item WRAPPED_STONE_HOE = registerItem("wrapped_stone_hoe", new Item(new Item.Settings()));
+    public static final Item WRAPPED_WOODEN_HOE = registerItem("wrapped_wooden_hoe", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Giftwrap.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
+            entries.add(GIFT_BUNDLE);
+            entries.add(GIFT_CHEST);
+            entries.add(GIFT_BLOCK);
+            entries.add(SMALL_GIFT);
+            entries.add(MEDIUM_GIFT);
+            entries.add(LARGE_GIFT);
+            entries.add(WRAPPED_WAND);
+            entries.add(WRAPPED_DIAMOND_SWORD);
+            entries.add(WRAPPED_DIAMOND_AXE);
+            entries.add(WRAPPED_DIAMOND_PICKAXE);
+            entries.add(WRAPPED_DIAMOND_SHOVEL);
+            entries.add(WRAPPED_GOLDEN_SWORD);
+            entries.add(WRAPPED_GOLDEN_AXE);
+            entries.add(WRAPPED_GOLDEN_PICKAXE);
+            entries.add(WRAPPED_GOLDEN_SHOVEL);
+            entries.add(WRAPPED_IRON_SWORD);
+            entries.add(WRAPPED_IRON_AXE);
+            entries.add(WRAPPED_IRON_PICKAXE);
+            entries.add(WRAPPED_IRON_SHOVEL);
+            entries.add(WRAPPED_STONE_SWORD);
+            entries.add(WRAPPED_STONE_AXE);
+            entries.add(WRAPPED_STONE_PICKAXE);
+            entries.add(WRAPPED_STONE_SHOVEL);
+            entries.add(WRAPPED_WOODEN_SWORD);
+            entries.add(WRAPPED_WOODEN_AXE);
+            entries.add(WRAPPED_WOODEN_PICKAXE);
+            entries.add(WRAPPED_WOODEN_SHOVEL);
+            entries.add(WRAPPED_DIAMOND_HOE);
+            entries.add(WRAPPED_GOLDEN_HOE);
+            entries.add(WRAPPED_IRON_HOE);
+            entries.add(WRAPPED_STONE_HOE);
+            entries.add(WRAPPED_WOODEN_HOE);
+        });
+    }
 }

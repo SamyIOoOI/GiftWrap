29dccfa58c89f07bc9ddaab098ec0159a70e7fe9
Samy0_o <97963987+SamyIOoOI@users.noreply.github.com>
2026-01-26 21:08:44 +0200

    fixing issues related to ModBlocks.java

--- src/main/java/com/hackwacky/giftwrap/block/ModBlocks.java
+++ src/main/java/com/hackwacky/giftwrap/block/ModBlocks.java
@@ -9,19 +9,25 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
-    public static final Block GIFT_BLOCK = registerBlock("gift_block", new Block(AbstractBlock.Settings.create().strength(4f).sounds(BlockSoundGroup.SNOW)));
+    public static final Block GIFT_BLOCK = registerBlock("gift_block", AbstractBlock.Settings.create().strength(4f).sounds(BlockSoundGroup.SNOW));
 
-    private static Block registerBlock(String name, Block block){
-        registerBlockItem(name, block);
-        return Registry.register(Registries.BLOCK, Identifier.of(Giftwrap.MOD_ID, name), block);
+    private static Block registerBlock(String name, AbstractBlock.Settings blockSettings){
+        RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(Giftwrap.MOD_ID, name));
+        Block block = new Block(blockSettings.registryKey(key));
+        registerBlockItem(name,block);
+        return Registry.register(Registries.BLOCK, key, block);
     }
 
     private static void registerBlockItem(String name, Block block){
-        Registry.register(Registries.ITEM, Identifier.of(Giftwrap.MOD_ID, name), new BlockItem(block, new Item.Settings()));
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Giftwrap.MOD_ID, name));
+        BlockItem item = new BlockItem(block, new Item.Settings().registryKey(key));
+        Registry.register(Registries.ITEM, key, item);
     }
     public static void registerModBlocks(){
         Giftwrap.LOGGER.info("Registering Mod Blocks for" + Giftwrap.MOD_ID);
